6172995fbf564d365b0d44e592ca3607b76d1380
Chris MacLeod <cmacleod@airdat.com>
2011-06-24 15:52:04 -0400

    new bundle

--- template/template.pl
+++ template/template.pl
@@ -1,15 +1,44 @@
+:silent! %s/\['filename'\]/\=expand('%:p:t')/g
 #!/usr/bin/perl
 
 use strict;
 use warnings;
 use Data::Dumper;
-use Getopt::Long qw( auto_help );
+use Pod::Usage;
+use Getopt::Long qw(:config auto_help auto_version );
 
-my $usage = <<EOU;
-Usage:
-  $0 --db DSN
-  $0 --action [hide|show]
-EOU
+$main::VERSION = "1.0";
+
+=head1 NAME
+
+['filename'] - a script
+
+=head1 SYNOPSIS
+
+  ['filename'] [options]
+
+  Options:
+    --help brief help message
+    --man full documentation
+
+=head1 OPTIONS
+
+=over
+
+=item B<--help>
+
+Print a brief help message and exits.
+
+=item B<--man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+=cut
 
 my %opts;
-GetOptions(\%opts, "db=s", "action=s") or die $usage;
+GetOptions(\%opts, "man", "db=s", "action=s" ) or pod2usage(2);
+pod2usage(-exitstatus => 0, -verbose => 2) if $opts{'man'};
